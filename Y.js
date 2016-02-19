d9a016ad242d3e6ae21bbb7c78ec75c1a320d8f8
Conor O'Brien <conorobrien4god@gmail.com>
2016-02-18 20:10:33 -0500

    fix op for fold

--- Y.js
+++ Y.js
@@ -234,7 +234,7 @@ function Y(code){
 			y.stack.push(t);
 		},
 		"|": function(y){
-			var op = y.links[y.curLink][++y.index];
+			var op = y.commands[y.links[y.curLink][++y.index]];
 			while(y.stack.length>1){
 				op(y);
 			}
